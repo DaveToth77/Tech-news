03d261da9c3aadfa6ba84820478756a4b0ab49cb
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-12 17:26:15 -0400

    Created the Login Route for Authentication

--- models/User.js
+++ models/User.js
@@ -3,8 +3,13 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
-
+// create our User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 User.init(
   {
     id: {
