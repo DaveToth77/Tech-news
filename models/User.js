0d9081a956e5e6129158bd61bb67caac9c86dd34
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-12 10:41:59 -0400

    created api routes

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Model, Datatypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our User model
@@ -7,45 +7,49 @@ class User extends Model {}
 // define table columns and configuration
 User.init(
     {
-        id: {
-            type: DataType.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        username: {
-            type: DataType.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataType.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            }
-        },
-        password: {
-            type: DataType.STRING,
-            allowNull: false,
-            validate: {
-               len: [4] 
-            }
+      // define an id column
+      id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        // instruct that this is the Primary Key
+        primaryKey: true,
+        // turn on auto increment
+        autoIncrement: true
+      },
+      // define a username column
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      // define an email column
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // there cannot be any duplicate email values in this table
+        unique: true,
+        // if allowNull is set to false, we can run our data through validators before creating the table data
+        validate: {
+          isEmail: true
         }
-},
-{
-    //Table configuration options go here
-    sequelize,
-    //pass in our imported sequelize connection ( the direct connection to our database) sequelize,
-    //dont automatically create createdAt/updateAt timestamp fields
-    timestamps: false,
-    //dont pluralize name of database table
-    freezeTableName: true,
-    //use underscores instead of camelcasing
-    underscored: true,
-   // make it so our model stays lowercase in the database
-   modelName: 'User'
-}
-);
-
+      },
+      // define a password column
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          // this means the password must be at least four characters long
+          len: [4]
+        }
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
 module.exports = User;
